cb176f0d5269be9f3e0bfca113564dd82d94e1c6
0xpantera <0xpantera@proton.me>
2023-07-04 12:02:37 -0600

    add message component and lipsum functions

--- packages/runtime/src/h.js
+++ packages/runtime/src/h.js
@@ -34,3 +34,22 @@ export function hFragment(vNodes) {
         children: mapTextNodes(withoutNulls(vNodes)),
     }
 }
+
+function MessageComponent({ level, message }) {
+    return h(
+        "div",
+        { class: `message message--${level}` },
+        [msg.message]
+    )
+}
+
+function lipsum(n) {
+    const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat.`
+
+    return hFragment(
+        Array(n).fill(h("p", {}, [text]))
+    )
+}
