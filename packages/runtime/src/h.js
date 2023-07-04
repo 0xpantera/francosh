be596359718335e0c23f78f4f0551082f7b16019
0xpantera <0xpantera@proton.me>
2023-07-04 10:15:40 -0600

    add functions to work on hypertext

--- packages/runtime/src/h.js
+++ packages/runtime/src/h.js
@@ -14,3 +14,23 @@ export function h(tag, props = {}, children = []) {
         type: DOM_TYPES.ELEMENT,
     }
 }
+
+function mapTextNodes(children) {
+    return children.map((child) =>
+        typeof child === "string" ? hString(child) : child
+    )
+}
+
+export function hString(str) {
+    return {
+        type: DOM_TYPES.TEXT,
+        value: str
+    }
+}
+
+export function hFragment(vNodes) {
+    return {
+        type: DOM_TYPES.FRAGMENT,
+        children: mapTextNodes(withoutNulls(vNodes)),
+    }
+}
